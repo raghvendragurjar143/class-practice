dc717f4fdfa1441c6c913638475811e85853e138
raghvendragurjar143 <raghvendragurjar143@gmail.com>
2026-06-22 15:37:54 +0530

    new file inserted

---
+++ tupple.py
@@ -0,0 +1,22 @@
+#defination and properties of tuple.
+#1.tuple is a data structurein python used to store miltiple data of different types with comma (,)in round brackets ().
+#waf to remove last word from tuple.
+
+
+
+
+
+
+
+#1. waf to sum of indices of tuple.
+marks_tuple = (10, 20, 30, 40, 50)
+s=0
+for i in  range (len(marks_tuple)):
+    s += i
+print(s)
+
+#indexing and slicing of tuple.
+marks_tuple = (10, 20, 30, 40, 50)
+print(marks_tuple[-1]) 
+print(marks_tuple[::-1])
+
